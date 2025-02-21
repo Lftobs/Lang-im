aecbc102e59bc76caedbb815fb8cbfa2d63e81e3
lftobs <tobbytobs1@gmail.com>
2025-02-21 22:46:07 +0100

    chore: added origin trial

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const originMetaDetect = document.createElement('meta');
+originMetaDetect.httpEquiv = 'origin-trial';
+originMetaDetect.content = import.meta.env.VITE_DETECTOR_TOKEN;
+document.head.append(originMetaDetect);
+
+const originMetaTranslate = document.createElement('meta');
+originMetaTranslate.httpEquiv = 'origin-trial';
+originMetaTranslate.content = import.meta.env.VITE_TRANSLATOR_TOKEN;
+document.head.append(originMetaTranslate);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
